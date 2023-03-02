c6391e17c244f73ee2c739990f8c8abe1ab3da04
JamesYUN <semicol11@gmail.com>
2023-03-02 17:58:41 +0900

    1st at Metable office

--- terrazzo/config.h
+++ terrazzo/config.h
@@ -28,8 +28,10 @@
 
 // SWAP FOR PRO MICRO
 // #define MATRIX_ROW_PINS { D2, D7, E6, B4, B5, B6, B2, B3 }
-#define MATRIX_ROW_PINS { B9, A1, A2, A3, A4, A5, A6, A7, B12 }
-#define MATRIX_COL_PINS { B0, B1, B10, B13, B14, B15 }
+#define MATRIX_ROW_PINS \
+    { B9, A1, A2, A3, A4, A5, A6, A7, B12 }
+#define MATRIX_COL_PINS \
+    { B0, B1, B10, B13, B14, B15 }
 #define DIODE_DIRECTION ROW2COL
 
 /* ROTARY ENCODERS */
@@ -56,8 +58,8 @@ so there is only one configuration. */
 
 /* TOP 3
    Default configuration */
-//#define ENCODERS_PAD_A { C6 , B7, C7 }
-//#define ENCODERS_PAD_B { D4 , D5, F1 }
+// #define ENCODERS_PAD_A { C6 , B7, C7 }
+// #define ENCODERS_PAD_B { D4 , D5, F1 }
 
 /* BOTTOM 3 */
 // #define ENCODERS_PAD_A { B7, C7, C6 }
@@ -67,15 +69,17 @@ so there is only one configuration. */
 // #define ENCODERS_PAD_A { C7, C6 }
 // #define ENCODERS_PAD_B { F1, D4 }
 
-
-//#define ENCODER_RESOLUTION 2
+// #define ENCODER_RESOLUTION 2
 #define BACKLIGHT_LEVELS 5
 
-//#ifdef LED_MATRIX_ENABLE
+// #ifdef LED_MATRIX_ENABLE
 
 #define LED_DRIVER_ADDR_1 0x74
 #define LED_DRIVER_COUNT 1
-#define DRIVER_LED_TOTAL 105
+
+// #define DRIVER_LED_TOTAL 105
+#define LED_MATRIX_LED_COUNT 105
+
 #define LED_MATRIX_ROWS 15
 #define LED_MATRIX_COLS 7
 #define LED_MATRIX_MAXIMUM_BRIGHTNESS 200
@@ -106,14 +110,14 @@ so there is only one configuration. */
 // #    endif
 // #    define ENABLE_LED_MATRIX_WAVE_LEFT_RIGHT
 // #    define ENABLE_LED_MATRIX_WAVE_UP_DOWN
-//#endif
+// #endif
 
 /* Terrazzo animations */
 
 // #define DISABLE_TERRAZZO_EFFECT_STRIPES
 // #define DISABLE_TERRAZZO_EFFECT_DINO
 // #define DISABLE_TERRAZZO_EFFECT_OUTRUN
-//            #define DISABLE_TERRAZZO_EFFECT_PAC_DUDE
-//            #define DISABLE_TERRAZZO_EFFECT_HEART
+#define DISABLE_TERRAZZO_EFFECT_PAC_DUDE
+#define DISABLE_TERRAZZO_EFFECT_HEART
 // #define DISABLE_TERRAZZO_EFFECT_WPM_CHART
-//            #define DISABLE_TERRAZZO_EFFECT_DOT
+#define DISABLE_TERRAZZO_EFFECT_DOT
